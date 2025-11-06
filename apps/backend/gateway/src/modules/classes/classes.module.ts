4066b78ee221dbcfdbc0237bda8ce9b6a68125f3
ilya1805-coder <kubanov1805@gmail.com>
2025-11-07 01:08:32 +0200

    Classes service (#4)
    * Classes service
    
    * Removed not needed files
    
    ---------
    
    Co-authored-by: Ilya <ilya@Ilyas-MacBook-Air.local>

--- apps/backend/gateway/src/modules/classes/classes.module.ts
+++ apps/backend/gateway/src/modules/classes/classes.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ClassesService } from './classes.service';
 import { ClassesResolver } from './classes.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'CLASSES_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: process.env.CLASSES_SERVICE_NAME,
+          port: process.env.CLASSES_SERVICE_PORT
+            ? parseInt(process.env.CLASSES_SERVICE_PORT)
+            : 3000,
+        },
+      },
+    ]),
+  ],
   controllers: [],
   providers: [ClassesResolver, ClassesService],
 })
